c65e043820402bac0e816ad900977229ef899629
Wilson Santos <wilson.saantos@hotmail.com>
2021-12-30 03:19:36 -0300

    Fix Deploy Asset

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,10 +5,10 @@ export function Header(prop: {black: boolean}) {
     return (
         <header className={prop.black ? 'black' : ''}>
             <div className="header--logo">
-                <a href="/"><img src="src\assets\logo\netflixlogo.png" alt="logo"></img></a> 
+                <a href="/"><img src="./src/assets/logo/netflixlogo.png" alt="logo"></img></a> 
             </div>
             <div className="header--user">
-                <a href="https://github.com/wilsaantos"><img src="src\assets\eu.jpg" alt="user"></img></a>
+                <a href="https://github.com/wilsaantos"><img src="./src/assets/eu.jpg" alt="user"></img></a>
             </div>
         </header>
     )
